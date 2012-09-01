b74e3927b3b5558f4fd94ef582703d57d81fef75
fukayatsu <fukayatsu@gmail.com>
2012-09-01 13:34:03 +0900

    総計取得の仮実装

--- lib/vending-machine.rb
+++ lib/vending-machine.rb
@@ -1,5 +1,8 @@
 # coding: utf-8
 
 class VendingMachine
+  def total_amount
+    0
+  end
 
 end
--- spec/vending-machine_spec.rb
+++ spec/vending-machine_spec.rb
@@ -3,9 +3,11 @@
 require 'vending-machine'
 
 describe VendingMachine do
-  context "総計取得テスト" do
-    it 'お金を投入していない時は総計0円' do
-      0.should == 1
+  context "お金を投入していない時" do
+    it '総計0円' do
+      machine = VendingMachine.new
+      machine.total_amount.should == 0
+
     end
   end
 
