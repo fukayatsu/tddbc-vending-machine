550b0eed349f90ebb00bd850ad9222b7c3ba783f
fukayatsu <fukayatsu@gmail.com>
2012-09-01 13:41:52 +0900

    投入の仮実装

--- lib/vending-machine.rb
+++ lib/vending-machine.rb
@@ -1,8 +1,15 @@
 # coding: utf-8
 
 class VendingMachine
+  def initialize
+    @total_amount = 0
+  end
   def total_amount
-    0
+    @total_amount
+  end
+
+  def insert(money)
+    @total_amount = money
   end
 
 end
--- spec/vending-machine_spec.rb
+++ spec/vending-machine_spec.rb
@@ -7,8 +7,13 @@ describe VendingMachine do
     it '総計0円' do
       machine = VendingMachine.new
       machine.total_amount.should == 0
-
     end
   end
-
+  context "十円投入した場合" do
+    it '総計十円' do
+      machine = VendingMachine.new
+      machine.insert(10)
+      machine.total_amount.should == 10
+     end
+  end
 end
